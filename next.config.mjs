7fa03c5ab0a6bfe9b443cefaad452306d3daaf69
alph4nov <mahfuzizzat0@gmail.com>
2026-01-09 23:03:14 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // <--- MANDATORY: Turns your app into static HTML files for GitHub
-  basePath: '/data-structures-main', // <--- MANDATORY: actual repo name (with the slash)
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true, // <--- MANDATORY: GitHub Pages cannot optimize images, this fixes broken images
-  },
-  experimental: {
-    webpackBuildWorker: true,
-  },
-}
+};
 
 export default nextConfig;
